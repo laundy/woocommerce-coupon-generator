9415959ecaae2f4adc689f583ffa54aebb4b455f
laundy <developing@laundy.de>
2021-07-06 21:29:03 +0200

    Handle responses

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
-import config from "./config";
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+const config = require('./config');
+const Coupon = require('./lib/Coupon');
 
 const api = new WooCommerceRestApi({
     url: config.url,
@@ -7,3 +8,34 @@ const api = new WooCommerceRestApi({
     consumerSecret: config.consumerSecret,
     version: config.version
 });
+
+const data = {
+    create: [{
+        code: "20off",
+        discount_type: "percent",
+        amount: "20",
+        individual_use: true,
+        exclude_sale_items: true,
+        minimum_amount: "100.00"
+    }],
+    update: [],
+    delete: []
+};
+
+let generatedCoupons = [];
+
+api.post("coupons/batch", data)
+    .then((response) => {
+        for (let createdCoupon of response.data.create) {
+            if (createdCoupon.error) {
+                // TODO: handle error
+                console.error(createdCoupon.error);
+            }
+            let coupon = new Coupon(createdCoupon);
+            generatedCoupons.push(coupon);
+            console.log(`Successfully created coupon ${coupon.code} (${coupon.amount} %) for customer ${coupon.description}`);
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
